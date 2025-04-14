22e6133b34b5d0a74525a8368d861d56dfad3736
Stroyoes <mazky7804@gmail.com>
2025-04-14 22:55:26 +0530

    Add score

--- src/components/Player.js
+++ src/components/Player.js
@@ -72,5 +72,8 @@ export function stepCompleted() {
   // Add new rows if the player is running out of them
   if (position.currentRow > rows.length - 10) addRows();
 
+  const scoreDOM = document.getElementById("score");
+  if (scoreDOM) scoreDOM.innerText = position.currentRow.toString();
+
 }
 
