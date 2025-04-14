976a5100509a0c137a6873634e7761fb9ce292b0
Stroyoes <mazky7804@gmail.com>
2025-04-14 17:55:02 +0530

    Add all the objects as static

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,6 +7,20 @@ import { Truck } from "./Truck";
 
 export const metadata = [
   {
+    type: "forest",
+    trees: [
+      { tileIndex: -3, height: 50 },
+      { tileIndex: 2, height: 30 },
+      { tileIndex: 5, height: 50 },
+    ],
+  },
+  {
+    type: "car",
+    direction: false,
+    speed: 1,
+    vehicles: [{ initialTileIndex: 2, color: 0xff0000 }],
+  },
+  {
     type: "truck",
     direction: true,
     speed: 0,
